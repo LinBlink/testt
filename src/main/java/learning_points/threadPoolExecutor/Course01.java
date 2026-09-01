e11a4519aa158ee0f87627bb11c6a626eee7bfcc
LinBlink <827650791@qq.com>
2026-09-01 08:46:15 +0800

    update

---
+++ src/main/java/learning_points/threadPoolExecutor/Course01.java
@@ -0,0 +1,12 @@
+package learning_points.threadPoolExecutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Course01 {
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+
+    }
+}
